effee441fb487f9d5704e41a7a71f3de3c973066
OdinsBeard82 <danielcharles_500@hotmail.com>
2023-11-23 21:48:55 +0000

    getting images added

---
+++ script.js
@@ -0,0 +1,3 @@
+    let imageElement = document.createElement('img');
+    imageElement.src = './images/pexels-life-of-pix-7974.jpg';
+    document.getElementsById('imageContainer').appendChild(imageElement);
