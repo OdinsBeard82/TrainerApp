bc408148c7545c200ef16a24981e85e0ee04b0b4
OdinsBeard82 <danielcharles_500@hotmail.com>
2023-12-02 21:40:02 +0000

    training styles adeed and styled

--- javascript/One-to-One.js
+++
@@ -1,9 +0,0 @@
-function trainerAppButton(One) {
-    document.getElementById(One).onclick = homePage;
-}
-
-function homePage() {
-    window.location.href = '../module/One-to-One.html';
-}
-
-trainerAppButton("oneToOne");
--- script.js
+++ script.js
@@ -1,37 +1,38 @@
 document.addEventListener('DOMContentLoaded', function () {
+    setupButtonClick("trainerApp");
     setupButtonClick("getStartedButton");
     setupButtonClick("getStartedButton2");
-    trainerAppButton("trainerApp");
-    OneToOneButton("oneToOneLink"); 
+    setupButtonClick("oneToOneLink");
+    setupButtonClick("nutritionLink");
+    setupButtonClick("onlineLink");
+    setupButtonClick("pricing");
+    setupButtonClick("about"); 
+    setupButtonClick("signIn");  
 });
 
 function setupButtonClick(buttonId) {
-    document.getElementById(buttonId).onclick = newPage;
+    document.getElementById(buttonId).onclick = function () {
+        navigateToPage(buttonId);
+    };
 }
 
-function newPage() {
-    window.location.href = './module/getStarted.html';
+function navigateToPage(buttonId) {
+    const pageMap = {
+        trainerApp: './index.html',
+        getStartedButton: './module/getStarted.html',
+        getStartedButton2: './module/getStarted.html',
+        oneToOneLink: './module/One-to-One.html',
+        nutritionLink: './module/nutritionPage.html',
+        onlineLink: './module/online.html',
+        pricing: './module/pricing.html',
+        about: './module/about.html',
+        signIn: './module/signIn.html'
+    };
+
+    const pageUrl = pageMap[buttonId];
+    if (pageUrl) {
+        window.location.href = pageUrl;
+    } else {
+        console.error(`Page URL not defined for button with ID: ${buttonId}`);
+    }
 }
-
-function trainerAppButton(buttonId) {
-    document.getElementById(buttonId).onclick = homePage;
-}
-
-function homePage() {
-    window.location.href = './index.html';
-}
-
-function OneToOneButton(buttonId) {
-    document.getElementById(buttonId).onclick = OneToOne;
-}
-
-function OneToOne() {
-    window.location.href = './module/One-to-One.html';
-}
-
-
-
-
-
-
-
