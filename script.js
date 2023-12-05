fa15c7989f4b8d499764a397194152f4bf37d1ea
OdinsBeard82 <danielcharles_500@hotmail.com>
2023-12-05 13:23:04 +0000

    slider , getStarted buttons as well as drpdwn menu

--- script.js
+++ script.js
@@ -85,3 +85,20 @@ function navigateToPage(buttonId) {
         imageElement.src = "../images/profit.jpg";
 
     }
+
+    function toggleDropdown() {
+        var dropdown = document.getElementById("myDropdown");
+        dropdown.classList.toggle("show");
+    }
+
+    window.onclick = function(event) {
+        if (!event.target.matches('.dropdown button')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            for (var i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
