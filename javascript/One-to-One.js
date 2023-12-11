75dbdb179b00f280b739b30b71fe0eb3dff6d87b
OdinsBeard82 <danielcharles_500@hotmail.com>
2023-12-11 10:10:55 +0000

    5 getstarted buttons done in one-to-one

--- javascript/One-to-One.js
+++ javascript/One-to-One.js
@@ -4,7 +4,8 @@ document.addEventListener('DOMContentLoaded', function () {
     setupButtonClick("getStartedButton3");
     setupButtonClick("getStartedButton4");
     setupButtonClick("getStartedButton5");
-
+    setupButtonClick("getStartedButton6");
+    setupButtonClick("getStartedButton7");
 });
 
 function setupButtonClick(buttonId) {
@@ -18,8 +19,9 @@ function navigateToPage(buttonId) {
         trainerApp2: '../index.html',
         getStartedButton3: '../module/getStarted.html',
         getStartedButton4: '../module/getStarted.html',
-        getStartedButton5: '../module/getStarted.html'
-
+        getStartedButton5: '../module/getStarted.html',
+        getStartedButton6: '../module/getStarted.html',
+        getStartedButton7: '../module/getStarted.html'
     };
 
     const pageUrl = pageMap[buttonId];
