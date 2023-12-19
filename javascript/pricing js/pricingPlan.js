23e1b24904cee9006725f72862f71036d956201b
OdinsBeard82 <danielcharles_500@hotmail.com>
2023-12-19 08:11:23 +0100

    just in case sve

--- javascript/pricing js/pricingPlan.js
+++ javascript/pricing js/pricingPlan.js
@@ -49,6 +49,7 @@ function changeSlider2() {
     var monthlyPricing2 = document.getElementById("monthlyPricingPlan");
     var free2Element = document.getElementById("free2");
     var free3Element = document.getElementById("free3");
+    var free4Element = document.getElementById("free4");
 
 
 
@@ -57,6 +58,8 @@ function changeSlider2() {
     monthlyPricing2.style.fontWeight = checkbox.checked ? "normal" : "bold"; 
     free2Element.textContent = checkbox.checked ? "£14.99" : "£16.99";
     free3Element.textContent = checkbox.checked ? "£26.99" : "£29.99";
+    free4Element.textContent = checkbox.checked ? "£225" : "£249";
+
 
 
 
